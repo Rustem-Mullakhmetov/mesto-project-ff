f85e31f55baf3944a4e8e94c8ee7a0753596d680
Rustem M. <Rustem.M.S@ya.ru>
2024-05-21 12:39:34 +0300

    Проект Место. Первая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,14 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('.places__list');
+const card = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+    const cardElement = card.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (event) {
+      const cardDelete = event.target.closest('.card');
+      cardDelete.remove();
+    });
+    cardTemplate.append(cardElement);
+});
